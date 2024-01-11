0073fc94af8dd8513d9e7d0fe63cd5de92208798
Aju Abraham <ajubabraham23@gmail.com>
2024-01-11 13:48:44 +0530

    set routes for user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import { registerUser,loginUser,logOutUser, refreshAcessToken, changePassword, currentUser, updateAccountDetial, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
+import { registerUser,
+    loginUser,
+    logOutUser, 
+    refreshAcessToken, 
+    changePassword, 
+    currentUser, 
+    updateAccountDetial,
+    updateUserAvatar,
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getUserWatchHistroy } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 const userRoutes = Router();
@@ -23,7 +33,9 @@ userRoutes.route('/logout').post(verifyJwt,logOutUser)
 userRoutes.route('/refresh-token').post(refreshAcessToken)
 userRoutes.route('/change-password').post(verifyJwt,changePassword)
 userRoutes.route('/current-user').get(verifyJwt,currentUser)
-userRoutes.route('/update-account-detail').post(verifyJwt,updateAccountDetial)
-userRoutes.route('/update-avatar').post(verifyJwt,upload.single('avatar'),updateUserAvatar)
-userRoutes.route('/update-coverImage').post(verifyJwt,upload.single('coverImage'),updateUserCoverImage)
+userRoutes.route('/update-account-detail').patch(verifyJwt,updateAccountDetial)
+userRoutes.route('/update-avatar').patch(verifyJwt,upload.single('avatar'),updateUserAvatar)
+userRoutes.route('/update-coverImage').patch(verifyJwt,upload.single('coverImage'),updateUserCoverImage)
+userRoutes.route('/c/:username').get(verifyJwt,getUserChannelProfile)
+userRoutes.route('/history').get(verifyJwt,getUserWatchHistroy)
 export default userRoutes;
